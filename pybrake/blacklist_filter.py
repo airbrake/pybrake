3411d67b90f7703366ab8d34ca26b38122ffd52b
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2018-08-10 10:52:43 +0300

    Test on Python 3.7

--- pybrake/blacklist_filter.py
+++ pybrake/blacklist_filter.py
@@ -11,26 +11,37 @@ def make_blacklist_filter(keys_blacklist):
   def blacklist_filter(notice):
     for key in _NOTICE_KEYS:
       if key in notice:
-        filter_dict(notice[key], keys_blacklist)
+        _filter_dict(notice[key], keys_blacklist)
     return notice
 
   return blacklist_filter
 
 
-def filter_dict(d, keys_blacklist):
+def _filter_dict(d, keys_blacklist):
   for key, value in d.items():
-    if is_blacklisted(key, keys_blacklist):
+    if _is_blacklisted(key, keys_blacklist):
       d[key] = _FILTERED
       continue
     if isinstance(value, collections.abc.Mapping):
-      filter_dict(value, keys_blacklist)
+      _filter_dict(value, keys_blacklist)
 
 
-def is_blacklisted(key, keys_blacklist):
+def _is_blacklisted(key, keys_blacklist):
   for k in keys_blacklist:
     if k == key:
       return True
-    if isinstance(k, re._pattern_type):
-      if k.match(key):
-        return True
+    if _is_regexp(k) and k.match(key):
+      return True
   return False
+
+
+try:
+  _pattern_type = re._pattern_type
+except AttributeError:
+  try:
+    _pattern_type = re.Pattern
+  except AttributeError:
+    pass
+
+def _is_regexp(v):
+  return isinstance(v, _pattern_type)
