55faae5fa1f23911bfde9ee7910abc8db19bd583
Anatolii Mihailenco <anmic.webdev@gmail.com>
2019-09-19 14:36:33 +0300

    flask: add request fitler

--- pybrake/flask.py
+++ pybrake/flask.py
@@ -28,6 +28,44 @@ from . import metrics
 _UNKNOWN_ROUTE = "UNKNOWN"
 
 
+def request_filter(notice):
+    if request is None:
+        return notice
+
+    ctx = notice["context"]
+    ctx["method"] = request.method
+    ctx["url"] = request.url
+    ctx["route"] = str(request.endpoint)
+
+    try:
+        user_addr = request.access_route[0]
+    except IndexError:
+        user_addr = request.remote_addr
+    if user_addr:
+        ctx["userAddr"] = user_addr
+
+    if _flask_login_available and current_user.is_authenticated:
+        user = dict(id=current_user.get_id())
+        for s in ["username", "name"]:
+            if hasattr(current_user, s):
+                user[s] = getattr(current_user, s)
+        ctx["user"] = user
+
+    notice["params"]["request"] = dict(
+        form=request.form,
+        json=request.json,
+        files=request.files,
+        cookies=request.cookies,
+        headers=dict(request.headers),
+        environ=request.environ,
+        blueprint=request.blueprint,
+        url_rule=request.url_rule,
+        view_args=request.view_args,
+    )
+
+    return notice
+
+
 def init_app(app):
     if "pybrake" in app.extensions:
         raise ValueError("pybrake is already injected")
@@ -36,6 +74,8 @@ def init_app(app):
 
     notifier = Notifier(**app.config["PYBRAKE"])
     apm_disabled = notifier._apm_disabled
+
+    notifier.add_filter(request_filter)
 
     app.extensions["pybrake"] = notifier
     got_request_exception.connect(_handle_exception, sender=app)
@@ -93,37 +133,6 @@ def _handle_exception(sender, exception, **_):
     notifier = sender.extensions["pybrake"]
 
     notice = notifier.build_notice(exception)
-    ctx = notice["context"]
-    ctx["method"] = request.method
-    ctx["url"] = request.url
-    ctx["route"] = str(request.endpoint)
-
-    try:
-        user_addr = request.access_route[0]
-    except IndexError:
-        user_addr = request.remote_addr
-    if user_addr:
-        ctx["userAddr"] = user_addr
-
-    if _flask_login_available and current_user.is_authenticated:
-        user = dict(id=current_user.get_id())
-        for s in ["username", "name"]:
-            if hasattr(current_user, s):
-                user[s] = getattr(current_user, s)
-        ctx["user"] = user
-
-    notice["params"]["request"] = dict(
-        form=request.form,
-        json=request.json,
-        files=request.files,
-        cookies=request.cookies,
-        headers=dict(request.headers),
-        environ=request.environ,
-        blueprint=request.blueprint,
-        url_rule=request.url_rule,
-        view_args=request.view_args,
-    )
-
     notifier.send_notice(notice)
 
 
