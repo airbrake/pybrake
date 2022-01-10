7f95626684cf524feb81043629cab903f3cff64d
Saloni.Jain <Saloni.Jain@logicmonitor.com>
2022-01-10 22:18:42 +0530

    Convert notifier class to singleton.

--- pybrake/notifier.py
+++ pybrake/notifier.py
@@ -39,7 +39,18 @@ _CONTEXT = dict(
 )
 
 
-class Notifier:
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Notifier(metaclass=Singleton):
+
     def __init__(
         self, *, project_id=0, project_key="", host="https://api.airbrake.io", **kwargs
     ):
