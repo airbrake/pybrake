c5a588dbdd8ea5362a1137df3cd961b970c40f4d
Kaushal Prajapati <kbprajapati@live.com>
2022-05-04 18:06:12 +0530

    [FIX] package import

--- examples/django/weather/settings.py
+++ examples/django/weather/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pybrake.middleware.django.AirbrakeMiddleware',
+    'pybrake.django.AirbrakeMiddleware',
 ]
 
 ROOT_URLCONF = 'weather.urls'
