ba46fdcbc119f601b58ae73ee76ebe7ca396755c
Saloni Jain <80541125+salonijain0918@users.noreply.github.com>
2022-05-09 14:04:07 +0530

    Sample Bottle Application (#172)
    * Sample Bottle Application
    
    * [IMP]bottle framework example and error
    
    * [IMP]example based middleware
    
    * [IMP]example app
    
    Co-authored-by: Saloni.Jain <Saloni.Jain@logicmonitor.com>
    Co-authored-by: Kaushal Prajapati <kbprajapati@live.com>

---
+++ examples/bottle/README.md
@@ -0,0 +1,66 @@
+# Bottle Sample Application for Pybrake
+
+## About the application:
+
+The example application provides three GET endpoints
+
+1. `/date` - returns server date and time.
+2. `/locations` - returns list of available locations.
+3. `/weather/{locationName}` - returns the weather details for the locations.
+
+## Steps to run the API
+
+1. Install the dependencies for the application
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+2. You must get both `project_id` & `project_key`.
+
+   To find your `project_id` and `project_key` from Airbrake account and
+   replace it in below code in your project's `main.py` file.
+
+    ```python
+    from bottle import run, response, Bottle
+    from pybrake.middleware.bottle import init_app
+   
+    app = Bottle()
+   
+    app.config['PYBRAKE'] = dict(
+       project_id=999999,                               # Insert your Project Id here
+       project_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',  # Insert your Project Key here
+       environment="test",
+       performance_stats=True,                          # False to disable APM
+   )
+   
+    app = init_app(app)
+    ```
+
+3. Run the localhost server
+
+    ```bash
+    python main.py
+    ```
+
+4. To retrieve the responses, append the endpoints to the localhost URL.
+
+   Use the below curl commands to interact with the endpoints.
+
+    ```bash
+    curl "http://localhost:3000/date"
+    curl "http://localhost:3000/locations"
+    curl "http://localhost:3000/weather/<austin/pune/santabarbara>"
+    ```
+
+   The below curl command will raise `404 Not Found` error.
+
+    ```bash
+    curl -I "http://localhost:3000/weather"
+    ```
+
+   The below curl command will raise `500 Internal server error` error.
+
+    ```bash
+    # Should produce an intentional HTTP 500 error and report the error to Airbrake (since `washington` is in the supported cities list but there is no data for `washington`, an `if` condition is bypassed and the `data` variable is used but not initialized)
+    curl -I "http://localhost:3000/weather/washington"
+    ```
---
+++ examples/bottle/main.py
@@ -0,0 +1,92 @@
+import requests
+from time import sleep
+from random import randrange
+from datetime import datetime
+from bottle import run, response, Bottle
+from pybrake.middleware.bottle import init_app
+from bottle.ext import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, Sequence, String
+from sqlalchemy.ext.declarative import declarative_base
+
+app = Bottle()
+
+app.config['PYBRAKE'] = dict(
+    project_id=415799,
+    project_key='ad3b547922fb0a691c6e7a5a5842b8b7',
+)
+
+Base = declarative_base()
+engine = create_engine('sqlite:////tmp/test.db', echo=True)
+
+app.install(sqlalchemy.Plugin(
+    engine,  # SQLAlchemy engine created with create_engine function.
+    Base.metadata,  # SQLAlchemy metadata, required only if create=True.
+    keyword='db',  # Keyword used to inject session database in a route (
+    # default 'db').
+    create=True,  # If it is true, execute `metadata.create_all(engine)`
+    # when plugin  is applied (default False).
+    commit=True,  # If it is true, plugin commit changes after route is
+    # executed (default True).
+    use_kwargs=False  # If it is true and keyword is not defined, plugin
+    # uses **kwargs argument to inject session database (default False).
+))
+app = init_app(app)
+
+city_list = ["austin", "pune", "santabarbara", "washington"]
+
+
+class Entity(Base):
+    __tablename__ = 'entity'
+    id = Column(Integer, Sequence('id_seq'), primary_key=True)
+    name = Column(String(50))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Entity('%d', '%s')>" % (self.id, self.name)
+
+
+# API for Hello Application
+@app.get('/')
+def hello(db):
+    sleep(randrange(0, 3))
+    print(db.query(Entity).all())
+    return "Hello, Welcome to the Weather App!"
+
+
+# API for current server date
+@app.get('/date', method=['GET'])
+def getdate():
+    return {
+        "date": "Current date and time is: %s" % datetime.now()
+    }
+
+
+# API for location details
+@app.get('/locations')
+def get_location_details():
+    return {
+        'cities': city_list
+    }
+
+
+# API for weather details for a location
+@app.get('/weather/<location_name>')
+def get_weather_details(location_name):
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Cache-Control'] = 'no-cache'
+    if location_name not in city_list:
+        response.status = 400
+        return {
+            'error': 'Not found: Location not found!'
+        }
+    with requests.get(
+            'https://airbrake.github.io/weatherapi/weather/' +
+            location_name) as f:
+        data = f.json()
+        return data
+
+
+if __name__ == '__main__':
+    run(app, host='localhost', port=3000, debug=True, reloader=True)
