1168774185e6ef9b87dd1960dc3e4ede510d86d1
Kaushal Prajapati <kbprajapati@live.com>
2022-10-19 17:09:42 +0530

    Release v1.10.0 :tada:

--- src/pybrake/constant.py
+++ src/pybrake/constant.py
@@ -2,7 +2,7 @@
 notifier_name = "pybrake"
 
 # Pybrake Notifier Version
-version = "1.9.0"
+version = "1.10.0"
 
 # End point of Airbrake API host. That will always be as default value.
 AIRBRAKE_HOST = "https://api.airbrake.io"
